25fbf38fa89020435f320676f78aa9cd67b34d66
Santiago Canal <santycanal_06@hotmail.com>
2024-05-30 06:55:59 -0500

    changes in de datgabase config adding maxidletime

--- database/database.go
+++ database/database.go
@@ -34,7 +34,8 @@ func NewMysqlUserDb(ctx context.Context) {
 
 		DBusers.SetMaxOpenConns(config.Config.MySQLConnection.Weaver.MaxOpenConns)
 		DBusers.SetMaxIdleConns(config.Config.MySQLConnection.Weaver.MaxIdleConns)
-		//DBusers.SetConnMaxLifetime(time.Duration(config.Config.MySQLConnection.Weaver.ConnMaxLifeTime) * time.Second)
+		DBusers.SetConnMaxLifetime(time.Duration(config.Config.MySQLConnection.Weaver.ConnMaxLifeTime) * time.Second)
+		DBmessage.SetConnMaxIdleTime(15 * time.Minute)
 
 		if err = DBusers.Ping(); err != nil {
 			ins_log.Fatalf(ctx, "cant do ping to the database error :%s", err)
@@ -55,7 +56,8 @@ func NewMysqlMessageDb(ctx context.Context) {
 
 		DBmessage.SetMaxOpenConns(config.Config.MySQLConnection.Raven.MaxOpenConns)
 		DBmessage.SetMaxIdleConns(config.Config.MySQLConnection.Raven.MaxIdleConns)
-		//DBmessage.SetConnMaxLifetime(time.Duration(config.Config.MySQLConnection.Raven.ConnMaxLifeTime) * time.Second)
+		DBmessage.SetConnMaxLifetime(time.Duration(config.Config.MySQLConnection.Raven.ConnMaxLifeTime) * time.Second)
+		DBmessage.SetConnMaxIdleTime(15 * time.Minute)
 
 		if err = DBmessage.Ping(); err != nil {
 			ins_log.Fatalf(ctx, "cant do ping to the database error :%s", err)
